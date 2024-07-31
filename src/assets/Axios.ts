8d5876b4ee3da5fccdb9074a610ac268822c0ff8
blanco <dehara14@gmail.com>
2024-07-30 21:34:20 -0600

    añadido validacion de sesion en axios

--- src/assets/Axios.ts
+++ src/assets/Axios.ts
@@ -3,12 +3,20 @@ import axios from "axios";
 const BASE_URL = "http://192.168.100.4:4000";
 export const urlSocket = "ws://localhost:4001";
 
+const { token }: { token: { token: string | null } } = localStorage.getItem(
+  "credentials"
+)
+  ? JSON.parse(String(localStorage.getItem("credentials")))
+  : { token: { token: null } };
+
 export default axios.create({
   baseURL: `${BASE_URL}/api/`,
-  headers: { "Content-Type": "application/json" },
+  headers: { "Content-Type": "application/json", Authorization: token.token },
   validateStatus: (status: number) => {
-    if (status === 401) {
-      localStorage.removeItem("Auth");
+    console.log(status);
+
+    if (status === 401 || status === 403) {
+      localStorage.removeItem("credentials");
       window.location.href = "/";
     }
     return true;
