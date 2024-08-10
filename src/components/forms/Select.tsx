a4e241c9d7a135ec142206eafc8f0fc36352620a
blanco <dehara14@gmail.com>
2024-08-10 13:08:45 -0600

    Select empleado y react select

--- src/components/forms/Select.tsx
+++ src/components/forms/Select.tsx
@@ -1,6 +1,8 @@
 import moment from "moment";
-import { FC, ReactNode, useState } from "react";
+import { FC, ReactNode, useMemo, useState } from "react";
 import { meses } from "../../assets/misc";
+import { useGetData } from "../../hooks/useGetData";
+import RSelect from "react-select";
 
 export const Select: FC<{
   name: string;
@@ -118,6 +120,109 @@ export const SelectYear: FC<{
         <option>{actualValue}</option>
         <option>{actualValue + 1}</option>
       </select>
+    </label>
+  );
+};
+
+//http://localhost:4000/api/empleado?departamento=1&auth=false&estatus=1&estatus=6
+
+export const SelectEmpleado: FC<{
+  name: string;
+  label: string;
+  variable: any;
+  setVariable: any;
+  required?: boolean;
+  disabled?: boolean;
+  estatus: string[];
+  departamento:
+    | "0"
+    | "1"
+    | "2"
+    | "3"
+    | "4"
+    | "5"
+    | "6"
+    | "7"
+    | "8"
+    | "9"
+    | "10"
+    | "11"
+    | "12";
+}> = ({
+  name,
+  label,
+  variable,
+  setVariable,
+  disabled,
+  required,
+  estatus,
+  departamento,
+}) => {
+  const { data, isPending, isFetching, isError } = useGetData(
+    `empleado?departamento=${
+      departamento ? departamento : "1"
+    }&auth=false&estatus=${estatus ? estatus.join("&estatus=") : ""}`,
+    "empledoSelectData"
+  );
+
+  return (
+    <label className="form-control w-full max-w-40 lg:max-w-xs">
+      <div className="label">
+        <span className="label-text">{label}</span>
+      </div>
+      <select
+        className="select select-bordered"
+        name={name}
+        onChange={(ev) => {
+          const { name, value } = ev.currentTarget;
+          setVariable((prev: any) => ({ ...prev, [name]: value }));
+        }}
+        value={variable.hasOwnProperty(name) ? variable[name] : ""}
+        disabled={disabled}
+        required={required}
+      >
+        <option disabled value="">
+          Selecciona un empleado
+        </option>
+        {!isPending &&
+          !isFetching &&
+          data.response.map(
+            (el: { nombre_completo: string; idempleado: number }) => (
+              <option value={el.idempleado} key={el.idempleado}>
+                {el.nombre_completo}
+              </option>
+            )
+          )}
+      </select>
+    </label>
+  );
+};
+
+export const ReactSelect: FC = () => {
+  return (
+    <label className="form-control w-full max-w-40 lg:max-w-xs">
+      <div className="label">
+        <span className="label-text">Etiqueta de ejemplo</span>
+      </div>
+      <RSelect
+        classNames={{
+          control: (state) =>
+            `bg-base-100 rounded-btn border-base-content/20 border h-12 min-h-12 w-full ps-4 pe-10 text-sm ${
+              state.isFocused
+                ? "outline outline-2 outline-base-content/20 outline-offset-2"
+                : ""
+            }`,
+          menu: () => `bg-base-200`,
+        }}
+        unstyled
+        styles={{
+          control: (baseStyles) => {
+            const { outline, ...base } = baseStyles;
+            return { ...base };
+          },
+        }}
+        noOptionsMessage={(value) => "Sin Opciones"}
+      />
     </label>
   );
 };
