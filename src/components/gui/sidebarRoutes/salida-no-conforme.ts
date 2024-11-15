50dbad76ab91e332dd62df505a9475c56f439c02
Jose Luis Cabrera Aguirre <aj3amdua@gmail.com>
2024-11-15 16:14:40 -0600

    Ruta de SNC

--- src/components/gui/sidebarRoutes/salida-no-conforme.ts
+++ src/components/gui/sidebarRoutes/salida-no-conforme.ts
@@ -21,68 +21,13 @@ export default {
       //     name: "Registros checklist",
       //   },
       // ],
+      show: true,
     },
-
     {
-      to: "orden-limpieza-isla",
-      name: "Órden y limpieza isla",
-      icon: "spray-can-sparkles",
-      collapse: [
-        { to: "capturar", name: "Capturar", show: true },
-        {
-          to: "reporte",
-          name: "Reporte",
-          show: true,
-        },
-        { to: "tendencia", name: "Tendencia", show: true },
-        /* { to: "despacho/orden-limpieza/reporte", name: "Reporte mensual" },
-        {
-          to: "despacho/orden-limpieza/historial",
-          name: "Historial",
-        },
-        {
-          to: "despacho/orden-limpieza/tendencia",
-          name: "Tendencia",
-        }, */
-      ],
+      to: "archivos",
+      name: "Archivos",
+      icon: "file-pdf",
+      show: true,
     },
-    /*
-      {
-        to: "evaluacion-uniforme",
-        name: "Evaluación de uniforme",
-        icon: "shirt",
-        collapse: [
-          { to: "despacho/evaluacion-uniforme/capturar", name: "Capturar" },
-          {
-            to: "despacho/evaluacion-uniforme/capturarreporte",
-            name: "Reporte mensual",
-          },
-          {
-            to: "despacho/evaluacion-uniforme/capturarhistorial",
-            name: "Historial",
-          },
-          {
-            to: "despacho/evaluacion-uniforme/capturartendencia",
-            name: "Tendencias",
-          },
-        ],
-      },
-      {
-        to: "pasos-despachar",
-        name: "Pasos de despacho",
-        icon: "list-check",
-        collapse: [
-          { to: "despacho/pasos-despachar/capturar", name: "Capturar" },
-          { to: "despacho/pasos-despachar/reporte", name: "Reporte mensual" },
-          {
-            to: "despacho/pasos-despachar/historial",
-            name: "Historial",
-          },
-          {
-            to: "despacho/pasos-despachar/tendencia",
-            name: "Tendencia",
-          },
-        ],
-      }, */
   ],
 } satisfies sideBarItems;
